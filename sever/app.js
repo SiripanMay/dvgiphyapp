778d7c479513005a87366ff4d2e2bd9d1328705b
SiripanMay <sirian_saecheah@cmu.ac.th>
2019-02-07 02:09:08 +0700

    version1

--- sever/app.js
+++ sever/app.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
+const express = require('express')
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000
 
-const app = express();
-
-app.use(
-  morgan(
-    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
-  )
-);
-
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.get('/', (req, res) => {
+  res.json({
+    message: 'OK'
+  })
+})
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
+  res.json({
+    message: 'Error'
+  })
+})
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+  console.log(`Server is listening on ${PORT}`)
+})
